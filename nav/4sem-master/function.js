f3ec4e6be835d809941c70618cf8ee791699078c
rikk626a <32450104+rikk626a@users.noreply.github.com>
2020-02-10 08:14:01 +0100

    start-git

---
+++ nav/4sem-master/function.js
@@ -0,0 +1,315 @@
+$(".csp").click(function () {
+     $(".csp").addClass("active");
+     $(".csp > .icon").addClass("active");
+     $(".backup").removeClass("active");
+     $(".sikkerhed").removeClass("active");
+     $(".addins").removeClass("active");
+     $(".gdpr").removeClass("active");
+     $(".migrering").removeClass("active");
+     $(".remote").removeClass("active");
+     $(".crm").removeClass("active");
+     $(".backup > .icon").removeClass("active");
+     $(".sikkerhed > .icon").removeClass("active");
+     $(".addins > .icon").removeClass("active");
+     $(".gdpr > .icon").removeClass("active");
+     $(".migrering > .icon").removeClass("active");
+     $(".remote > .icon").removeClass("active");
+     $(".crm > .icon").removeClass("active");
+     $("#line").addClass("one");
+     $("#line").removeClass("two");
+     $("#line").removeClass("three");
+     $("#line").removeClass("four");
+     $("#line").removeClass("five");
+     $("#line").removeClass("six");
+     $("#line").removeClass("seven");
+     $("#line").removeClass("eight");
+
+})
+
+$(".backup").click(function () {
+     $(".csp").removeClass("active");
+     $(".csp > .icon").removeClass("active");
+     $(".backup").addClass("active");
+     $(".sikkerhed").removeClass("active");
+     $(".addins").removeClass("active");
+     $(".gdpr").removeClass("active");
+     $(".migrering").removeClass("active");
+     $(".remote").removeClass("active");
+     $(".crm").removeClass("active");
+     $(".backup > .icon").addClass("active");
+     $(".sikkerhed > .icon").removeClass("active");
+     $(".addins > .icon").removeClass("active");
+     $(".gdpr > .icon").removeClass("active");
+     $(".migrering > .icon").removeClass("active");
+     $(".remote > .icon").removeClass("active");
+     $(".crm > .icon").removeClass("active");
+     $("#line").addClass("two");
+     $("#line").removeClass("one");
+     $("#line").removeClass("three");
+     $("#line").removeClass("four");
+     $("#line").removeClass("five");
+     $("#line").removeClass("six");
+     $("#line").removeClass("seven");
+     $("#line").removeClass("eight");
+
+})
+
+$(".sikkerhed").click(function () {
+     $(".csp").removeClass("active");
+     $(".csp > .icon").removeClass("active");
+     $(".backup").removeClass("active");
+     $(".sikkerhed").addClass("active");
+     $(".addins").removeClass("active");
+     $(".gdpr").removeClass("active");
+     $(".migrering").removeClass("active");
+     $(".remote").removeClass("active");
+     $(".crm").removeClass("active");
+     $(".backup > .icon").removeClass("active");
+     $(".sikkerhed > .icon").addClass("active");
+     $(".addins > .icon").removeClass("active");
+     $(".gdpr > .icon").removeClass("active");
+     $(".migrering > .icon").removeClass("active");
+     $(".remote > .icon").removeClass("active");
+     $(".crm > .icon").removeClass("active");
+     $("#line").addClass("three");
+     $("#line").removeClass("one");
+     $("#line").removeClass("two");
+     $("#line").removeClass("four");
+     $("#line").removeClass("five");
+     $("#line").removeClass("six");
+     $("#line").removeClass("seven");
+     $("#line").removeClass("eight");
+
+})
+
+$(".addins").click(function () {
+     $(".csp").removeClass("active");
+     $(".csp > .icon").removeClass("active");
+     $(".backup").removeClass("active");
+     $(".sikkerhed").removeClass("active");
+     $(".addins").addClass("active");
+     $(".gdpr").removeClass("active");
+     $(".migrering").removeClass("active");
+     $(".remote").removeClass("active");
+     $(".crm").removeClass("active");
+     $(".backup > .icon").removeClass("active");
+     $(".sikkerhed > .icon").removeClass("active");
+     $(".addins > .icon").addClass("active");
+     $(".gdpr > .icon").removeClass("active");
+     $(".migrering > .icon").removeClass("active");
+     $(".remote > .icon").removeClass("active");
+     $(".crm > .icon").removeClass("active");
+     $("#line").addClass("four");
+     $("#line").removeClass("one");
+     $("#line").removeClass("two");
+     $("#line").removeClass("three");
+     $("#line").removeClass("five");
+     $("#line").removeClass("six");
+     $("#line").removeClass("seven");
+     $("#line").removeClass("eight");
+
+})
+
+$(".gdpr").click(function () {
+     $(".csp").removeClass("active");
+     $(".csp > .icon").removeClass("active");
+     $(".backup").removeClass("active");
+     $(".sikkerhed").removeClass("active");
+     $(".addins").removeClass("active");
+     $(".gdpr").addClass("active");
+     $(".migrering").removeClass("active");
+     $(".remote").removeClass("active");
+     $(".crm").removeClass("active");
+     $(".backup > .icon").removeClass("active");
+     $(".sikkerhed > .icon").removeClass("active");
+     $(".addins > .icon").removeClass("active");
+     $(".gdpr > .icon").addClass("active");
+     $(".migrering > .icon").removeClass("active");
+     $(".remote > .icon").removeClass("active");
+     $(".crm > .icon").removeClass("active");
+     $("#line").addClass("five");
+     $("#line").removeClass("one");
+     $("#line").removeClass("two");
+     $("#line").removeClass("three");
+     $("#line").removeClass("four");
+     $("#line").removeClass("six");
+     $("#line").removeClass("seven");
+     $("#line").removeClass("eight");
+
+})
+
+$(".migrering").click(function () {
+     $(".csp").removeClass("active");
+     $(".csp > .icon").removeClass("active");
+     $(".backup").removeClass("active");
+     $(".sikkerhed").removeClass("active");
+     $(".addins").removeClass("active");
+     $(".gdpr").removeClass("active");
+     $(".migrering").addClass("active");
+     $(".remote").removeClass("active");
+     $(".crm").removeClass("active");
+     $(".backup > .icon").removeClass("active");
+     $(".sikkerhed > .icon").removeClass("active");
+     $(".addins > .icon").removeClass("active");
+     $(".gdpr > .icon").removeClass("active");
+     $(".migrering > .icon").addClass("active");
+     $(".remote > .icon").removeClass("active");
+     $(".crm > .icon").removeClass("active");
+     $("#line").addClass("six");
+     $("#line").removeClass("one");
+     $("#line").removeClass("two");
+     $("#line").removeClass("three");
+     $("#line").removeClass("four");
+     $("#line").removeClass("five");
+     $("#line").removeClass("seven");
+     $("#line").removeClass("eight");
+
+})
+
+$(".remote").click(function () {
+     $(".csp").removeClass("active");
+     $(".csp > .icon").removeClass("active");
+     $(".backup").removeClass("active");
+     $(".sikkerhed").removeClass("active");
+     $(".addins").removeClass("active");
+     $(".gdpr").removeClass("active");
+     $(".migrering").removeClass("active");
+     $(".remote").addClass("active");
+     $(".crm").removeClass("active");
+     $(".backup > .icon").removeClass("active");
+     $(".sikkerhed > .icon").removeClass("active");
+     $(".addins > .icon").removeClass("active");
+     $(".gdpr > .icon").removeClass("active");
+     $(".migrering > .icon").removeClass("active");
+     $(".remote > .icon").addClass("active");
+     $(".crm > .icon").removeClass("active");
+     $("#line").addClass("seven");
+     $("#line").removeClass("one");
+     $("#line").removeClass("two");
+     $("#line").removeClass("three");
+     $("#line").removeClass("four");
+     $("#line").removeClass("five");
+     $("#line").removeClass("six");
+     $("#line").removeClass("eight");
+
+})
+
+$(".crm").click(function () {
+     $(".csp").removeClass("active");
+     $(".csp > .icon").removeClass("active");
+     $(".backup").removeClass("active");
+     $(".sikkerhed").removeClass("active");
+     $(".addins").removeClass("active");
+     $(".gdpr").removeClass("active");
+     $(".migrering").removeClass("active");
+     $(".remote").removeClass("active");
+     $(".crm").addClass("active");
+     $(".backup > .icon").removeClass("active");
+     $(".sikkerhed > .icon").removeClass("active");
+     $(".addins > .icon").removeClass("active");
+     $(".gdpr > .icon").removeClass("active");
+     $(".migrering > .icon").removeClass("active");
+     $(".remote > .icon").removeClass("active");
+     $(".crm > .icon").addClass("active");
+     $("#line").addClass("eight");
+     $("#line").removeClass("one");
+     $("#line").removeClass("two");
+     $("#line").removeClass("three");
+     $("#line").removeClass("four");
+     $("#line").removeClass("five");
+     $("#line").removeClass("six");
+     $("#line").removeClass("seven");
+
+})
+
+
+
+
+$(".csp").click(function () {
+     $("#first").addClass("active");
+     $("#second").removeClass("active");
+     $("#third").removeClass("active");
+     $("#fourth").removeClass("active");
+     $("#fifth").removeClass("active");
+     $("#sixt").removeClass("active");
+     $("#seventh").removeClass("active");
+     $("#eigth").removeClass("active");
+})
+
+$(".backup").click(function () {
+     $("#first").removeClass("active");
+     $("#second").addClass("active");
+     $("#third").removeClass("active");
+     $("#fourth").removeClass("active");
+     $("#fifth").removeClass("active");
+     $("#sixt").removeClass("active");
+     $("#seventh").removeClass("active");
+     $("#eigth").removeClass("active");
+})
+
+$(".sikkerhed").click(function () {
+     $("#first").removeClass("active");
+     $("#second").removeClass("active");
+     $("#third").addClass("active");
+     $("#fourth").removeClass("active");
+     $("#fifth").removeClass("active");
+     $("#sixt").removeClass("active");
+     $("#seventh").removeClass("active");
+     $("#eigth").removeClass("active");
+})
+
+$(".addins").click(function () {
+     $("#first").removeClass("active");
+     $("#second").removeClass("active");
+     $("#third").removeClass("active");
+     $("#fourth").addClass("active");
+     $("#fifth").removeClass("active");
+     $("#sixt").removeClass("active");
+     $("#seventh").removeClass("active");
+     $("#eigth").removeClass("active");
+})
+
+$(".gdpr").click(function () {
+     $("#first").removeClass("active");
+     $("#second").removeClass("active");
+     $("#third").removeClass("active");
+     $("#fourth").removeClass("active");
+     $("#fifth").addClass("active");
+     $("#sixt").removeClass("active");
+     $("#seventh").removeClass("active");
+     $("#eigth").removeClass("active");
+})
+
+$(".migrering").click(function () {
+     $("#first").removeClass("active");
+     $("#second").removeClass("active");
+     $("#third").removeClass("active");
+     $("#fourth").removeClass("active");
+     $("#fifth").removeClass("active");
+     $("#sixt").addClass("active");
+     $("#seventh").removeClass("active");
+     $("#eigth").removeClass("active");
+})
+
+$(".remote").click(function () {
+     $("#first").removeClass("active");
+     $("#second").removeClass("active");
+     $("#third").removeClass("active");
+     $("#fourth").removeClass("active");
+     $("#fifth").removeClass("active");
+     $("#sixt").removeClass("active");
+     $("#seventh").addClass("active");
+     $("#eigth").removeClass("active");
+})
+
+$(".crm").click(function () {
+     $("#first").removeClass("active");
+     $("#second").removeClass("active");
+     $("#third").removeClass("active");
+     $("#fourth").removeClass("active");
+     $("#fifth").removeClass("active");
+     $("#sixt").removeClass("active");
+     $("#seventh").removeClass("active");
+     $("#eigth").addClass("active");
+     $("#defaultOpen").addClass("active");
+})
---
+++ nav/js.js
@@ -0,0 +1,46 @@
+// remove nav-o
+$(window).scroll(function () {
+
+     if ($(window).scrollTop() > 400) {
+          $('.nav-o').addClass('hidden');
+     } else {
+          $('.nav-o').removeClass('hidden');
+     }
+});
+
+// Sticky Header
+$(window).scroll(function () {
+
+     if ($(window).scrollTop() > 400) {
+          $('.main_h').addClass('sticky');
+     } else {
+          $('.main_h').removeClass('sticky');
+     }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function () {
+     if ($('.main_h').hasClass('open-nav')) {
+          $('.main_h').removeClass('open-nav');
+     } else {
+          $('.main_h').addClass('open-nav');
+     }
+});
+
+$('.main_h li a').click(function () {
+     if ($('.main_h').hasClass('open-nav')) {
+          $('.navigation').removeClass('open-nav');
+          $('.main_h').removeClass('open-nav');
+     }
+});
+
+// navigation scroll
+$('nav a').click(function (event) {
+     var id = $(this).attr("href");
+     var offset = 70;
+     var target = $(id).offset().top - offset;
+     $('html, body').animate({
+          scrollTop: target
+     }, 500);
+     event.preventDefault();
+});
